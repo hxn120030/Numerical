3f84ae5baa06febd63e07f1a051a5dad7a96fac9
hxn120030 <hxn120030@utdallas.edu>
2017-02-06 15:32:00 -0600

    Add files via upload

--- Homework/Hw3/CP6.m
+++ Homework/Hw3/CP6.m
@@ -1,15 +1,16 @@
 function [ y ] = CP6( x )
-    y1 = sin(x) - 1 + cos(x)
+    y1 = sin(x) - 1 + cos(x); %This function is the original function
     
-    a=0
-    for i = 1:6
+    % We now start using taylor series to to compute our orginal function.
+    a=0;
+    for i = 1:6 % We stop at 6 for single bit, if we wanted double we would use 11
         b = ((-1)^i)*x^(2*i)/(factorial(2*i));
-        a = a + b
+        a = a + b;
     end
     
-    y2 = sin(x) + a;
+    y2 = sin(x) + a; % This will give the vaule using the taylor series of the orginal function.
     
-    y=[y1,y2];
+    y=[y1,y2]; % We print out both vaules for easy comparison.
 
 end
 
